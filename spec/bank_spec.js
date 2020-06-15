79e60b4f4d3ca7ecb132ac778cbd8809fe99430c
Joanne Young <joanneyoung_@hotmail.com>
2020-06-15 14:49:07 +0100

    user can input and current date is accounted for

--- spec/bank_spec.js
+++ spec/bank_spec.js
@@ -5,6 +5,9 @@ describe("bank", function(){
   beforeEach(function(){
     bank = new Bank;
     bank.bankBalance = 0;
+    const fixedDate = new Date(2020, 01, 01);
+    jasmine.clock().install();
+    jasmine.clock().mockDate(fixedDate);
   })
  
   it("should add to the bank balance", function(){
@@ -15,5 +18,14 @@ describe("bank", function(){
     expect(bank.deposit(300)).toEqual(300)
   });
 
-});
+  it("can deposit with a date", function(){
+    Date.now()
+    bank.deposit(100)
+    expect(bank.dateFormat()).toEqual("1/1/2020")
+  });
 
+  afterEach(() => {
+    jasmine.clock().uninstall();
+  });
+
+});
--- src/bank.js
+++ src/bank.js
@@ -2,14 +2,26 @@ class Bank {
 
   constructor() {
     this.bankBalance = 0.00
+    this.date = 0
   }
 
-  format(number){
+  currencyFormat(number){
     return "£" + number.toFixed(2);
   }
 
+  dateFormat(){
+    var currentDate = new Date()
+    var day = currentDate.getDate()
+    var month = currentDate.getMonth()
+    var year = currentDate.getFullYear()
+    return day + "/" + month + "/" + year
+  }
+
   deposit(input) {
+    this.date = this.dateFormat()
     this.bankBalance += input
     return this.bankBalance
+    
   }
+
 }
